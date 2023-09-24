0d79fbcce760e2a5d68cb724806526913d52df98
Thiago Antonio <thiagoapsx@gmail.com>
2023-09-24 14:04:22 -0300

    segunda versão da aula, solução ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,25 +28,20 @@ public class Program {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation: " +reservation);
 			
-			System.out.println();
-			
+			System.out.println();			
 			System.out.println("Enter data to update the reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			
-			Date now = new Date(); //cria uma data atual.
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates.");				
+				
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
-			else if (!checkOut.after(checkIn)){
-				System.out.println("Error in reservation: Check-out date must be after check-in date.");
-			}
-			else {			
-			reservation.updateDates(checkIn, checkOut); // método responsável por atualizar as datas.
-			System.out.println("Reservation: " +reservation);
-			}
+			else {
+				System.out.println("Reservation: " + reservation);
+			}			
 		}
 		
 		System.out.println();
