552ad671a6808a36f282788f99a858c8431f353e
Thiago Antonio <thiagoapsx@gmail.com>
2023-09-24 15:36:55 -0300

    terceira versão da aula, solução boa

--- src/model/entites/Reservation.java
+++ src/model/entites/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainExeception;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -15,10 +17,18 @@ public class Reservation {
 	public Reservation () {		
 	}
 
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainExeception{
+		Date now = new Date();
+		
+		if(checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainExeception("Reservation dates for update must be future dates.");
+		}
+		if (!checkOut.after(checkIn)){
+			throw new DomainExeception("Check-out date must be after check-in date.");
+		}
 		this.roomNumber = roomNumber;
-		this.checkIn = checkin;
-		this.checkOut = checkout;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
 	public Integer getRoomNumber() {
@@ -42,20 +52,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);		
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainExeception{
 		
 		Date now = new Date();
 		
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates.";			
+			throw new DomainExeception("Reservation dates for update must be future dates.");
 		}
 		if (!checkOut.after(checkIn)){
-			return "Check-out date must be after check-in date.";
+			throw new DomainExeception("Check-out date must be after check-in date.");
 		}	
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	@Override
@@ -69,12 +78,5 @@ public class Reservation {
 				+ ", "
 				+ duration()
 				+ " nights.";
-	}
-
-
-	
-	
-	
-	
-
+	}	
 }
---
+++ src/model/exceptions/DomainExeception.java
@@ -0,0 +1,11 @@
+package model.exceptions;
+
+public class DomainExeception extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public DomainExeception(String msg) {
+		super(msg);
+	}
+	
+
+}
